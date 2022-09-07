cd0ce6e3b990b8bb8e28c80d0b666aa587bf296b
ninjafiveo <42717162+ninjafiveo@users.noreply.github.com>
2022-09-07 13:06:10 -0400

    While Loop

--- functions.py
+++ functions.py
@@ -1,1 +1,18 @@
 # DRY - Don't Repeat yourself.
+# DRY - Don't Repeat yourself.
+# DRY - Don't Repeat yourself.
+# DRY - Don't Repeat yourself.
+
+def directions():
+    print("Turn Left out of parking lot.")
+    print("Drive to stop light and turn left.")
+    print("Drive one mile through Canfield.")
+    print("Starbucks is across the street from Sheetz.")
+    print("Turn left into Starbucks")
+
+number_of_people = int(input("How many people are asking for directions? "))
+
+while number_of_people > 0:
+    print(f"{number_of_people} - New Directions")
+    directions()
+    number_of_people = number_of_people - 1
